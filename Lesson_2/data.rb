797dcae1c190254ca4734e167ad333931c715821
Irina Kudashkina <simurgy@mail.ru>
2016-01-20 01:08:01 +0700

    Add leap year algorithm

--- Lesson_2/data.rb
+++ Lesson_2/data.rb
@@ -6,11 +6,11 @@ puts "Введите число"
 day = gets.chomp.to_i
 
 months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-months[1] = 29 if (2016 - year) % 4 == 0 
+
+months[1] = 29 if (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)
 
 serial_number = day
 months.each_index  do |i| 
 	serial_number += months[i] if i + 1 < month
 end
 puts "Это #{serial_number}-й день года"
- 
